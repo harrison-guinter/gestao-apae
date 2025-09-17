45eb8573ea7ca382ed082e2093f1c0f4bc77ee63
Harrison Guinter Zimmer Dietze <harrison.dietze@campus.labs>
2025-09-17 20:16:21 -0300

    Feature: mensagens de alerta de login

--- SistemaApae.App/src/app/modules/auth/reset-password/reset-password.component.ts
+++ SistemaApae.App/src/app/modules/auth/reset-password/reset-password.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../auth.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-reset-password',
@@ -15,15 +16,21 @@ import { MatIconModule } from '@angular/material/icon';
 export class ResetPasswordComponent {
   protected resetPasswordForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private snackBar: MatSnackBar, private router: Router) {
     this.resetPasswordForm = this.fb.group({
-      email: ['', Validators.required]
+      email: ['', [Validators.required, Validators.email]],
     });
   }
 
   resetPassword(): void {
     if (this.resetPasswordForm.invalid) return;
     const { email } = this.resetPasswordForm.value;
-    this.authService.resetPassword(email).subscribe();
+    this.authService.resetPassword(email).subscribe(res => {
+      this.snackBar.open(res.message, '', { duration: 3000 });
+    });
+  }
+
+  goToLogin(): void {
+    this.router.navigate(['/login']);
   }
 }
