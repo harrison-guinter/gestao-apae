fe8b72bb86ee568b84f2d049869e0567aec9ac7d
Leandro Júnior Dias <leandrokjrdias@gmail.com>
2025-09-27 16:38:07 -0300

    Reapply "🎨 Improve ajuste do response do usuario"
    This reverts commit 6f26f9b68f8b5cf5da61ff62ae3a676b29f32264.

--- SistemaApae.Api/Repositories/Users/UsuarioRepository.cs
+++ SistemaApae.Api/Repositories/Users/UsuarioRepository.cs
@@ -126,12 +126,13 @@ public class UsuarioRepository : IUsuarioRepository
     /// Cria um novo usuário
     /// </summary>
     /// <returns> Usuario criado </returns>
-    public async Task<Usuario> CreateAsync(Usuario user)
+    public async Task<Usuario> CreateAsync(Usuario user, string hashedPassword)
     {
         try
         {
             user.Id = Guid.NewGuid();
             user.UpdatedAt = DateTime.UtcNow;
+            user.Senha = hashedPassword;
 
             var response = await _supabaseService.Client
                 .From<Usuario>()
@@ -154,12 +155,17 @@ public class UsuarioRepository : IUsuarioRepository
     {
         try
         {
-            user.UpdatedAt = DateTime.UtcNow;
-
             var response = await _supabaseService.Client
                 .From<Usuario>()
                 .Where(u => u.Id == user.Id)
-                .Update(user);
+                .Set(u => u.Nome, user.Nome)
+                .Set(u => u.Email, user.Email)
+                .Set(u => u.Telefone!, user.Telefone)
+                .Set(u => u.RegistroProfissional!, user.RegistroProfissional)
+                .Set(u => u.Especialidade!, user.Especialidade)
+                .Set(u => u.Observacao!, user.Observacao)
+                .Set(u => u.UpdatedAt, DateTime.UtcNow)
+                .Update();
 
             return response.Models.First();
         }
