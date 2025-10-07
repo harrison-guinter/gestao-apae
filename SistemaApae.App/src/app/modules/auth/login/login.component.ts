b4ea726880de28575086b8e891935740fbdf07f2
Rodfrei <rodorsf@gmail.com>
2025-10-06 21:28:36 -0300

    ✨ Criacao dos estilos do login e recuperar senha

--- SistemaApae.App/src/app/modules/auth/login/login.component.ts
+++ SistemaApae.App/src/app/modules/auth/login/login.component.ts
@@ -5,18 +5,32 @@ import { AuthService } from '../auth.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { NotificationService } from '../../core/notification/notification.service';
+import { InputComponent } from '../../core/input/input.component';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, MatButtonModule, MatInputModule, MatIconModule],
+  imports: [
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    InputComponent,
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less'],
 })
 export class LoginComponent {
   protected loginForm: FormGroup;
 
-  constructor(private router: Router, private fb: FormBuilder, private authService: AuthService, private notificationService: NotificationService) {
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private notificationService: NotificationService
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
