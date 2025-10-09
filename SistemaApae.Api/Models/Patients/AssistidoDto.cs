a40428a0ad4767d2cdceac4c97fa7591592006c2
paulohenz <paulo.henz.ph@gmail.com>
2025-10-08 22:38:34 -0300

    add dtos assistido e usuario

---
+++ SistemaApae.Api/Models/Patients/AssistidoDto.cs
@@ -0,0 +1,32 @@
+﻿using SistemaApae.Api.Models.Enums;
+
+namespace SistemaApae.Api.Models.Patients;
+
+public class AssistidoDto
+{
+
+    /// <summary>
+    /// ID único da entidade
+    /// </summary>
+    public Guid Id { get; set; }
+    /// <summary>
+    /// Nome do assistido
+    /// </summary>
+    public string Nome { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Indica se a entidade está ativa/inativa
+    /// </summary>
+    public StatusEntidadeEnum Status { get; set; }
+
+    /// <summary>
+    /// Nome do convênio do assistido
+    /// </summary>
+    public string NomeConvenio { get; set; }
+
+    /// <summary>
+    /// Nome da Cidade do assistido
+    /// </summary>
+    public string NomeCidade { get; set; }
+}
+
---
+++ SistemaApae.Api/Models/Users/UsuarioDto.cs
@@ -0,0 +1,62 @@
+﻿using SistemaApae.Api.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaApae.Api.Models.Users;
+public class UsuarioDto
+{
+
+    /// <summary>
+    /// ID único da entidade
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Nome do usuário
+    /// </summary>
+    [Required]
+    [MaxLength(150)]
+    public string Nome { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Email do usuário (único)
+    /// </summary>
+    [Required]
+    [EmailAddress]
+    [MaxLength(150)]
+    public string Email { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Telefone do usuário
+    /// </summary>
+    [MaxLength(20)]
+    public string? Telefone { get; set; }
+
+    /// <summary>
+    /// Perfil do usuário
+    /// </summary>
+    public PerfilEnum Perfil { get; set; }
+
+    /// <summary>
+    /// Registro profissional
+    /// </summary>
+    [MaxLength(50)]
+    public string? RegistroProfissional { get; set; }
+
+    /// <summary>
+    /// Especialidade do profissional
+    /// </summary>
+    [MaxLength(50)]
+    public string? Especialidade { get; set; }
+
+    /// <summary>
+    /// Observações sobre o usuário
+    /// </summary>
+    public string? Observacao { get; set; }
+
+    /// <summary>
+    /// Indica se a entidade está ativa/inativa
+    /// </summary>
+    public StatusEntidadeEnum Status { get; set; }
+
+}
+
